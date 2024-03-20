71e4fe89dbe57c388b0d90a5c1a6f370237efdf4
Konstanty <kostu96@gmail.com>
2024-03-20 20:13:31 +0100

    WIP

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,11 @@
   import type { User } from '@supabase/supabase-js'
   import { onMount } from 'svelte';
   import { supabase } from './supabaseClient'
-  import { Button, DarkMode, Label, Listgroup, ListgroupItem, Navbar } from 'flowbite-svelte';
+  import { Modal, Button, DarkMode, Label, Listgroup, Navbar, Footer, FooterCopyright } from 'flowbite-svelte';
   import { UserSolid } from 'flowbite-svelte-icons';
   import Auth from './components/Auth.svelte';
   import TodoItem from './components/TodoItem.svelte';
+  import TodoEdit from './components/TodoEdit.svelte';
 
   let user: User | null;
   let showTodoEdit: boolean = false;
@@ -17,7 +18,7 @@
     { id: 4, text: "todo4" },
   ];
 
-  const handleDelete = (id) => {
+  const handleDelete = (id: number) => {
     todos = todos.filter((todo) => todo.id != id);
   };
 
@@ -40,10 +41,10 @@
 </script>
 
 <main>
-  <Navbar class="flex">
+  <Navbar class="flex mb-4">
     <div class="grow">
       {#if user}
-        <Label class="flex">
+        <Label class="p-1 flex">
           <UserSolid />{user.email}
         </Label>
         <Button size="sm" 
@@ -51,7 +52,7 @@
             const { error } = await supabase.auth.signOut();
             if (error) console.log("Error logging out:", error.message);
           }}>Logout</Button>
-        <Button size="sm" on:click={() => showTodoEdit = !showTodoEdit}>New TODO</Button>
+        <Button size="sm" on:click={() => showTodoEdit = true}>New TODO</Button>
       {/if}
     </div>
     <DarkMode />
@@ -61,7 +62,7 @@
   {:else}
     <div class="flex flex-wrap">
       <Listgroup class="grow m-2">
-        <h3 class="p-1 text-center text-xl font-medium text-gray-900 dark:text-white">TODOs</h3>
+        <h3 class="py-1 px-20 text-center text-xl font-medium text-gray-900 dark:text-white">TODOs</h3>
         {#each todos as todo (todo.id)}
           <TodoItem on:delete={() => handleDelete(todo.id)}>{todo.text}</TodoItem>
         {:else}
@@ -77,5 +78,12 @@
         {/each}
       </Listgroup>
     </div>
+    <Modal bind:open={showTodoEdit}>
+      <TodoEdit />
+    </Modal>
   {/if}
+  <Footer>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <FooterCopyright href="https://github.com/Kostu96" target="_blank" by="Konstanty Misiak" />
+  </Footer>
 </main>
--- src/components/TodoEdit.svelte
+++ src/components/TodoEdit.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-
+	import { Button, Input, Textarea } from 'flowbite-svelte';
 </script>
 
-<form>
-	<h1>Add TODO</h1>
-	<div class="form-group">
-		<input class="form-control" placeholder="title" />
-		<textarea class="form-control" placeholder="description" />
-		<button type="button" class="btn btn-primary">Save</button>
-	</div>
+<form class="flex flex-col space-y-6" action="#">
+	<h3 class="text-xl font-medium text-gray-900 dark:text-white">New TODO</h3>
+	<Input type="text" placeholder="title" required />
+	<Textarea placeholder="description" />
+	<Button type="submit" class="w-full1">Add new TODO</Button>
 </form>
